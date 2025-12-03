d2879ba2d67eef27f7dfb692c737858df2accd7b
Cliq India <cliqindia@Snehams-MacBook-Pro.local>
2025-12-03 23:18:17 +0530

    edited server.js

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,19 @@ import wordpressRoutes from './routes/connectToWordpress.js';
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 const upload = multer({
   dest: "uploads/"   // folder where files will be stored
 });
@@ -55,19 +68,6 @@ app.use(cors({
 }));
 
 // app.options("*", cors());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
 
 
 
