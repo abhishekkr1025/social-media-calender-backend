6c76956cb777297a471713cc192072222de67d6b
Cliq India <cliqindia@Snehams-MacBook-Pro.local>
2025-12-05 16:18:29 +0530

    added new cors policy

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,32 @@ import wordpressRoutes from './routes/connectToWordpress.js';
 
 const app = express();
 
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Credentials", "true");
+
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+
+//   next();
+// });
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://social-media-calendar-frontend-ocxw.vercel.app/",
+  "https://prod.panditjee.com" // keep your main prod domain if needed
+];
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
