e73db204432f83febd6ac264ba3e51aaac59450e
Cliq India <cliqindia@Snehams-MacBook-Pro.local>
2026-01-12 16:09:48 +0530

    added multi-site

--- backend/server.js
+++ backend/server.js
@@ -1010,9 +1010,6 @@ app.use("/uploads", express.static("uploads"));
 
 
 
-const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("🚀 API server listening on", PORT);
-});
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => log(`API server listening on port ${PORT}`));
