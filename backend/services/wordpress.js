4e34e9b0f50cda26cf4835b43ddb19e926b25130
Cliq India <cliqindia@Snehams-MacBook-Pro.local>
2026-02-16 16:57:59 +0530

    reset wp-worker

--- backend/services/wordpress.js
+++ backend/services/wordpress.js
@@ -204,6 +204,9 @@ if (featured_media_id) {
     payload.date_gmt = new Date(iso).toISOString();
   }
 
+  log("FINAL PAYLOAD:", JSON.stringify(payload, null, 2));
+
+
   const response = await axios.post(
     `${site_url.replace(/\/$/, "")}/wp-json/wp/v2/posts`,
     payload,
